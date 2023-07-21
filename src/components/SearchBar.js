4eae91a48ff5a2125e57fff8b812dcbb294f348a
PoMaMa69 <saswatapatra15@gmail.com>
2023-07-21 16:52:17 +0530

    forms added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,21 @@
+import {useState} from 'react';
+
 function SearchBar({onSubmit}) {
-    const handleClick = () =>{
-        onSubmit('cars');
-    }
+    const [term,setTerm] = useState('');
+const handleFormSubmit =(event) =>{
+    event.preventDefault();
+    // console.log("hey do something");
+    onSubmit(term);
+}
+const handleChange = (event) =>{
+    // console.log(event.target.value);
+    setTerm(event.target.value);
+}
   return (
     <div>
-      <input></input>
-      <button onClick={handleClick}>click me!</button>
+      <form onSubmit={handleFormSubmit}>
+        <input value ={term} onChange={handleChange}></input>
+      </form>
     </div>
   );
 }
